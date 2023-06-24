4d5a4c72b9ea78a9abf3dd517ae78757db5a85ca
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-06-24 20:45:30 +0200

    Section 11 - Done

--- models/tourModels.js
+++ models/tourModels.js
@@ -158,13 +158,13 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
-//////Aggregation Middleware
+/* //////Aggregation Middleware
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   console.log(this.pipeline());
   next();
 });
-
+ */
 /////////////////////////
 
 const Tour = mongoose.model('Tour', tourSchema); //creating a model based on the schema
